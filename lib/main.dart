7cd58ffd94642fc12bdddf698e42d1c2f0744543
hunaina-nadeem <hunainanadeem0@gmail.com>
2023-07-18 22:14:38 +0500

    change name to filename

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'assesment_1/fi_bilal.dart';
+import 'package:flutter_interns/assesment_1/fi_hunaina.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FIBilal(),
+      home: FIHunaina(),
     );
   }
 }
