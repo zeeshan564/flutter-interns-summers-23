c9db8b32199f35b7a6a678f87fef9a4d50447687
bilalamjad0x01 <bilalamjad0x01@gmail.com>
2023-07-18 20:16:33 +0500

    Add "Bilal" as centered text in scaffold widget.

---
+++ lib/assesment_1/fi_bilal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FIBilal extends StatelessWidget {
+  const FIBilal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bilal"),
+      ),
+      body: const Center(
+        child: Text("Hello! I am Bilal"),
+      ),
+    );
+  }
+}
