d481509b950334fb4e50ee299c4247808a7d1542
Sadia-Saif <sadiasaif.official@gmail.com>
2023-07-18 17:36:37 +0500

    Assesment done

---
+++ lib/assesment_1/fi_sadia.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FIASadia extends StatelessWidget {
+  const FIASadia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sadia"),
+      ),
+      body: const Center(
+        child: Text("Hello! I am Sadia"),
+      ),
+    );
+  }
+}
