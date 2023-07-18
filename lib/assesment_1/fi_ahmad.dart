974e080c4793f8b5e7d48f5bec6b8f14a2148b41
Muhammad Zeeshan Tassawar <codechaser209@gmail.com>
2023-07-18 19:33:59 +0500

    Resolved Conflicts

--- lib/assesment_1/fi_ahmad.dart
+++ lib/assesment_1/fi_ahmad.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class FIZeeshan extends StatelessWidget {
-  const FIZeeshan({super.key});
+class FIAhmad extends StatelessWidget {
+  const FIAhmad({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Muhammad Zeeshan Tassawar'),
+        title: const Text("Ahmad"),
       ),
       body: const Center(
-        child: Text('Hi! I am Muhammad Zeeshan Tassawar.'),
+        child: Text("Hello! I am Ahmad"),
       ),
     );
   }
