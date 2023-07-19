8ead884ab98aeae6cf46c072fb1bf04298e7bb6a
jurazil <jurazilgreen@gmail.com>
2023-07-18 22:40:54 -0700

    initial com

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_interns/assesment_1/fi_ahmad.dart';
 import 'package:flutter_interns/assesment_1/fi_amna.dart';
 
 void main(List<String> args) {
