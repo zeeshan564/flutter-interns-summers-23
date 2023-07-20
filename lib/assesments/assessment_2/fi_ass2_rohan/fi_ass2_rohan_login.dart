5c12a06be57aff28f1eab2dd645ec8e1ba930efc
rohankhan <134016581+Rohankhan5990@users.noreply.github.com>
2023-07-20 09:50:31 +0500

    intail commit

---
+++ lib/assesments/assessment_2/fi_ass2_rohan/fi_ass2_rohan_login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Fia2rohanLogin extends StatelessWidget {
+  const Fia2rohanLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(width: 100, height: 100, color: Colors.amber));
+  }
+}
