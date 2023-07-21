9f289b567555335f7da8cc9ac0d8ae8b7cbace0b
jurazil <jurazilgreen@gmail.com>
2023-07-21 07:47:53 -0700

    added constants

--- lib/assesments/assessment_2/constants/f1_as2_amna_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class F1As2AmnaConstants {
-  static const primaryColor = Color(0xFFFA9884);
-}
---
+++ lib/assesments/assessment_2/constants/f1_as2_amna_constantd.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class F1As2AmnaConstants {
+  static const primaryColor = Color(0xFFFA9884);
+  static final back = SvgPicture.asset("assest/amna/Login screen logo.svg");
+
+  static final add = SvgPicture.asset("assest/svgs/add.svg");
+  static final home = SvgPicture.asset("assest/svgs/home.svg");
+  static final like = SvgPicture.asset("assest/svgs/Like.svg");
+  static final menu = SvgPicture.asset("assest/svgs/menu.svg");
+  static final more = SvgPicture.asset("assest/svgs/More.svg");
+  static final notification = SvgPicture.asset("assest/svgs/notification.svg");
+  static final notification_2 =
+      SvgPicture.asset("assest/svgs/notification_2.svg");
+  static final profile = SvgPicture.asset("assest/svgs/Profile.svg");
+  static final search = SvgPicture.asset("assest/svgs/Search.svg");
+  static final settings = SvgPicture.asset("assest/svgs/settings.svg");
+  static final share = SvgPicture.asset("assest/svgs/Share.svg");
+
+  static final cardImg = Image.asset("assest/images/card.png");
+  static final image_1 = Image.asset("assest/images/image1.png");
+  static final image_2 = Image.asset("assest/images/imag2.png");
+  static final image_3 = Image.asset("assest/images/image3.png");
+  static final image_4 = Image.asset("assest/images/image4.png");
+
+  static final girl_1 = Image.asset("assest/girl1.png");
+  static final girl_2 = Image.asset("assest/girl1.png");
+  static final man_1 = Image.asset("assest/girl1.png");
+}
