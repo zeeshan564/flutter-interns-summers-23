8a06f87c5dd993655530f71c4ff261b121898b0d
jurazil <jurazilgreen@gmail.com>
2023-07-22 04:42:14 -0700

    scroll card

--- lib/assesments/assessment_2/screens/home/fi_as2_amna_home.dart
+++ lib/assesments/assessment_2/screens/home/fi_as2_amna_home.dart
@@ -18,8 +18,16 @@ class F1As2AmnaHome extends StatelessWidget {
       F1As2AmnaAssets.girl_1,
       F1As2AmnaAssets.girl_2,
     ];
+
+    List<Widget> cards = [
+      const _F1As2AmnaCard(),
+      const _F1As2AmnaCard(),
+    ];
     Widget smallSpace = const SizedBox(
-      height: 40.0,
+      height: 10.0,
+    );
+    Widget mediumSpace = const SizedBox(
+      height: 32.0,
     );
     Widget imageWidth = const SizedBox(
       width: 32.0,
@@ -27,31 +35,42 @@ class F1As2AmnaHome extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(
-          top: 30.0,
-          right: 25.0,
-          left: 25.0,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 30.0,
+            right: 25.0,
+            left: 25.0,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(F1As2AmnaAssets.menu),
+                  SvgPicture.asset(F1As2AmnaAssets.notification),
+                ],
+              ),
+              mediumSpace,
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: generateImagesWithSeparator(images, imageWidth),
+                  )),
+              mediumSpace,
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: 505.0,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: generateCardsWithSeparator(cards, smallSpace),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SvgPicture.asset(F1As2AmnaAssets.menu),
-                SvgPicture.asset(F1As2AmnaAssets.notification),
-              ],
-            ),
-            smallSpace,
-            SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                    children: generateImagesWithSeparator(images, imageWidth))),
-            smallSpace,
-            const _F1As2AmnaCard(),
-          ],
-        ),
-      )),
+      ),
     );
   }
 
@@ -62,6 +81,14 @@ class F1As2AmnaHome extends StatelessWidget {
     List<Widget> widgetsWithSeparator =
         imageWidget.expand((widget) => [widget, separator]).toList();
     widgetsWithSeparator.removeLast(); // Remove the last separator at the end
+    return widgetsWithSeparator;
+  }
+
+  List<Widget> generateCardsWithSeparator(
+      List<Widget> cards, Widget separator) {
+    List<Widget> widgetsWithSeparator =
+        cards.expand((widget) => [widget, separator]).toList();
+
     return widgetsWithSeparator;
   }
 }
--- lib/assesments/assessment_2/screens/home/widget/_fi_as2_amna_card.dart
+++ lib/assesments/assessment_2/screens/home/widget/_fi_as2_amna_card.dart
@@ -1,114 +1,118 @@
 part of "../fi_as2_amna_home.dart";
 
 class _F1As2AmnaCard extends StatelessWidget {
-  const _F1As2AmnaCard({super.key});
+  const _F1As2AmnaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 498.0,
-      width: double.maxFinite,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(31, 0, 0, 0),
-            offset: Offset(0, 5),
-            blurRadius: 2.0,
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset("assets/girl2.png"),
-                const SizedBox(
-                  width: 15,
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      "Anton Demeron",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "@anton_demeron",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  width: 80.0,
-                ),
-                SvgPicture.asset(F1As2AmnaAssets.more),
-              ],
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            Image.asset(F1As2AmnaAssets.cardImg),
-            const SizedBox(
-              height: 5.0,
-            ),
-            Row(
-              children: [
-                const SizedBox(
-                  height: 50.0,
-                ),
-                SvgPicture.asset("assets/svgs/Like.svg"),
-                const SizedBox(
-                  width: 8,
-                ),
-                const Text(
-                  "573",
-                  style: TextStyle(fontSize: 11.5, fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  width: 20.0,
-                ),
-                SvgPicture.asset("assets/svgs/Coment.svg"),
-                const SizedBox(
-                  width: 8,
-                ),
-                const Text(
-                  "30",
-                  style: TextStyle(fontSize: 11.5, fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  width: 20.0,
-                ),
-                SvgPicture.asset(F1As2AmnaAssets.cardImg),
-                const Spacer(),
-                const Text(
-                  "35 min ago",
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Down the road",
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold),
-              ),
+    return SafeArea(
+      child: Container(
+        height: 498.0,
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(31, 0, 0, 0),
+              offset: Offset(0, 5),
+              blurRadius: 2.0,
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset("assets/girl2.png"),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Column(
+                    children: [
+                      Text(
+                        "Anton Demeron",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "@anton_demeron",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 80.0,
+                  ),
+                  SvgPicture.asset(F1As2AmnaAssets.more),
+                ],
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Image.asset(F1As2AmnaAssets.cardImg),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  SvgPicture.asset("assets/svgs/Like.svg"),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    "573",
+                    style:
+                        TextStyle(fontSize: 11.5, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  SvgPicture.asset("assets/svgs/Coment.svg"),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    "30",
+                    style:
+                        TextStyle(fontSize: 11.5, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  SvgPicture.asset(F1As2AmnaAssets.cardImg),
+                  const Spacer(),
+                  const Text(
+                    "35 min ago",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Down the road",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
