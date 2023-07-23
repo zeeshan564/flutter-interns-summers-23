1093f5858e81c84b3a659d852753243d49972b99
rohankhan <134016581+Rohankhan5990@users.noreply.github.com>
2023-07-23 18:03:24 +0500

    deleted files

--- lib/assesments/assessment_2/fi_ass2_rohan/Colors/fi_ass_rohan_colors.dart
+++
@@ -1,9 +0,0 @@
-import "package:flutter/material.dart";
-
-class MyColors {
-  static const grey = Color(0xFF9E9898);
-  static const lightgrey = Color(0xFFc8bfbf);
-  static const darkgrey = Color(0xFF575353);
-  static const black = Color(0xFF000000);
-  static const pink = Color(0xFFfa9884);
-}
