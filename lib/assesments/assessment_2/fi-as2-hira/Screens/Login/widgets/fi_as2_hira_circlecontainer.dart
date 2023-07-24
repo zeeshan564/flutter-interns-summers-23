1c9ffafc7b9e968d2780a2165bb3c9a4a6f322f8
Hira Tariq <i.hiratariq@gmail.com>
2023-07-24 10:42:23 +0500

    use own property of container done lib/assesments/assessment_2/fi-as2-hira/Screens/Login/widgets/fi_as2_hira_circlecontainer.dart

--- lib/assesments/assessment_2/fi-as2-hira/Screens/Login/widgets/fi_as2_hira_circlecontainer.dart
+++ lib/assesments/assessment_2/fi-as2-hira/Screens/Login/widgets/fi_as2_hira_circlecontainer.dart
@@ -10,15 +10,17 @@ class H1Circle extends StatelessWidget {
       height: 50,
       width: 50,
       decoration: BoxDecoration(
-          color: Constants.grey, borderRadius: BorderRadius.circular(50),),
-      padding: const EdgeInsets.all(3.0),
-      child: ClipRRect(
+        color: Constants.grey,
         borderRadius: BorderRadius.circular(50),
-        child: Container(
+      ),
+      padding: const EdgeInsets.all(3.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
           color: Colors.white,
-          padding: const EdgeInsets.all(2),
-          child: child,
         ),
+        padding: const EdgeInsets.all(2),
+        child: child,
       ),
     );
   }
